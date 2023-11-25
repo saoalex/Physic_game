75f53f16ae2f0c1566521ea6dcac3434da60c140
saoalex <150745867+saoalex@users.noreply.github.com>
2023-11-25 15:54:38 -0500

    Update testscript.mjs

--- testscript.mjs
+++ testscript.mjs
@@ -1,6 +1,90 @@
-// Start menu
+// Universal constants
+let resources = {
+  wealth: 100,
+  happiness: 100,
+  science: 100
+}
+
+const events = [
+  {
+    title: "Barbarians attack!",
+    description: "Your military has failed to defend your borders against barbarian tribes. Your state is being invaded and your great cities are being sacked! There is chaos on the streets and the barbarians are plundering your treasury. <br> Reduce gold: 5% <br> Reduce happiness: 50%",
+    effect: {wealth: 5, happiness: 50}
+  },
+  {
+    title: "Piracy and Thievery",
+    description: "It’s tough being rich. Pirates have attacked your convoy and stolen a great amount of treasure. Just hope it wasn’t the convoy with your greatest new technology! <br>Reduce gold: 8% <br>Reduce your happiness 10%",
+    effect: {wealth: 8, happiness: 10}
+  },
+  {
+    title: "War with neighbouring states",
+    description: "You have been dragged into a brutal war with your neighbours that leaves your society devastated and treasury depleted. Your peaceful focus on technological development is thrown into chaos. <br> Reduce gold: 30% <br> Reduce happiness: 40%",
+    effect: {wealth: 30, happiness: 40}
+  },
+  {
+    title:"Famine",
+    description: "A combination of drought and disease have blighted your crops. The harvest has failed again and your people are on the brink of starvation <br>Reduce gold:50% <br>Reduce happiness: 75%",
+    effect: {wealth: 50, happiness: 75}
+  },
+  {
+    title:"Plague",
+    description:"A deadly plague envelops your lands. It spreads like fire among the populace, killing countless scores and disabling many others. The destruction of productivity is profound. <br>Reduce gold: 30% <br>Reduce happiness: 75%",
+    effect:{wealth: 30, happiness: 75}
+  },
+  {
+    title:"Earhquake/Tsunami",
+    description: "Seismic activity below the Earth’s surface triggers a shock, either on land or on water. This results in much of your civilization being reduced to rubble. <br>Reduce gold: 40% <br>Reduce happiness: 50%",
+    effect:{wealth: 30, happiness: 50}
+  },
+  {
+    title:"Flood",
+    description: "Rising water levels flood your civilization, stranding and drowning many of your citizens. <br>Reduce gold: 35% <br>Reduce happiness: 45%",
+    effect: {wealth: 35, happiness: 45}
+  },
+  {
+    title:"Tropical cyclone",
+    description:"A tropical cyclone touches down on your civilization. The slew of thunderstorms and tornadoes damage much of your society. <br>Reduce gold: 25% <br>Reduce happiness: 35%",
+    effect: {wealth: 25, happiness: 35}
+  },
+  {
+    title: "Recession",
+    description: "Economic mismanagement and reckless speculation has your economy in shambles. The suffering populace start losing faith in your rule and are more reluctant to invest in frivolous research. <br>Reduce gold: 50% <br>Reduce happiness: 60%",
+    effect: {wealth: 50, happiness: 60}
+  },
+  {
+    title: "New land acquired",
+    description: "Congrats, you just acquired new lands and have expanded your nation! This brings new peoples and new fortunes, increasing the potential of your civilization. (Just don’t ask us whose land it is or how we acquired it…)<br>Increse gold: 15% <br>Increase happiness: 30%",
+    effect: {wealth: -15, happiness: -30}
+  },
+  {
+    title:"Higher birthrate",
+    description:"Something is in the air and more babies are being born than ever before! This baby boom brings some growing pains but will provide many new labourers and thinkers to increase the greatness of your civilization. <br>Increase gold: 10% <br>Increase happiness: 20%",
+    effect: {wealth: -10, happiness: -20}
+  },
+  {
+    title:"New technology from the foreign lands",
+    description:"Your traders discovered a new technology used in a distant land. They have brought it back for you to study and apply in your civilization. <br>Increase happiness: 10% <br>Unlocks new technology for free",
+    effect: {happiness: -10}
+  },
+  {
+    title:"Science prophet born",
+    description: "Some people are just born different. Such is the case with this prodigious child born in your capital. At the age of six, she is impressing your scholars. Her gifts will be numerous. <br>Increase happiness: 15% <br>Increase science: 15%",
+    effect: {happiness: -15, science:-15}
+  },
+  {
+    title: "Increase of state funding",
+    description: "The state recognizes the importance of basic research and opens its wallet to the desires of scientists and scholars. Others might feel neglected, though. <br>Increase gold: 30% <br>Increase science: 25% <br>Reduce happiness: 15%",
+    effect: {wealth: -30, happiness: 15, science: -25}
+  },
+  {
+    title: "Agriculture surplus",
+    description: "You have vast stores of crops, much more than what is necessary to feed your people at the moment. This attracts hungry stragglers, eager to bow to your rule for a morsel of food, and fellow leaders of civilizations, eager to buy crops from you to feed their people. <br>Increase gold: 35% <br>Increase happiness: 45%",
+    effect: {wealth: -35, happiness: -45}
+  }
+];
 
 
+// Initilization code of Start menu
 document.addEventListener('DOMContentLoaded', (event) => {
   /*
   Right now there is only a start button
@@ -17,30 +101,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 });
 
-import {updateCards, startCards, techCard} from './card.mjs';
-
-// Game Logic
 
 /*Event pop-up: random events should pop-up and take half of the screen when click on end turn button,  blurring the background. 
-
 To add events, copy-paste the title/description in the corresponding area in "const events".
 */
-const events = [
-  {
-    title: "Event 1",
-    description: "Description for Event 1... Oh my god this is such a long description I hope it doesn't mess up the display, say hows your day going?  I hope its good, cause mine is currently not going well. Have you eat? What did you eat? I bet its tasty. Anyways, What line am I on? I do hope it automatically changes."
-  },
-  {
-    title: "Event 2",
-    description: "Description for Event 2..."
-  },
-  {
-    title: "Event 3",
-    description: "Description for Event 3..."
-  },
-  // Add more events as needed
-];
 
+//fnuction to apply event affects
+function triggerEvent(event) {
+  // Apply the negative percentage effects on resources
+  for (const resource in event.effect) {
+    if (resources.hasOwnProperty(resource)) {
+      const decreaseAmount = resources[resource] * (event.effect[resource] / 100);
+      resources[resource] -= decreaseAmount;
+    }
+  }
+
+  // Ensure resources don't go negative
+  /*
+  for (const resource in resources) {
+    if (resources[resource] < 0) {
+      resources[resource] = 0;
+    }
+  }*/
+
+  // Update UI or other game elements based on new resource values
+  updateResourceDisplay();
+}
+
+//function to update the resources
+function updateResourceDisplay() {
+  // Example:
+  document.getElementById('wealthBar').textContent = Math.round(resources.wealth);
+  document.getElementById('happinessBar').textContent = Math.round(resources.happiness);
+  document.getElementById('scienceBar').textContent = Math.round(resources.science)
+}
 
 // Function to show the event when end turn button is clicked on.
 document.addEventListener('DOMContentLoaded', () => {
@@ -52,39 +146,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const storyArea = document.getElementById('eventArea');
 
   endTurnButton.addEventListener('click', () => {
-    // Select a random event
+    // Select a random event and trigger its effects
     const randomEvent = events[Math.floor(Math.random() * events.length)];
+    triggerEvent(randomEvent)
 
     // Update the modal content
     document.getElementById('event-title').textContent = randomEvent.title;
-    document.getElementById('event-description').textContent = randomEvent.description;
+    document.getElementById('event-description').innerHTML = randomEvent.description;
 
     // Show the modal
     eventModal.style.display = 'block';
-
-    // using updateCards to get stats for the turn and update the card list.
-    // newCards, statChanges, newDescriptions = updateCards(cardList);
-    
-
-    cardList.forEach(card => {
-      if (card._card.classList.contains('highlighted')) {
-        populateCards(card.hardChildren);
-        cardList.concat(card.hardChildren);
-        card._card.classList.remove('highlighted');
-        card.hide()
-      }
-    })
-
-    
   });
-
-
-
 
   const closeModal = () => {
     eventModal.style.display = 'none';
     // Append new event description to the existing content
-    const newEventDescription = `<p>Narrator: ${document.getElementById('event-description').textContent}</p>`;
+    const newEventDescription = `<p>Narrator: ${document.getElementById('event-title').textContent}</p>`;
     document.getElementById('eventArea').innerHTML += newEventDescription;
     // Scroll to the latest event description
     eventArea.scrollTop = eventArea.scrollHeight;
@@ -94,6 +171,17 @@ document.addEventListener('DOMContentLoaded', () => {
   closeEventButton.addEventListener('click', closeModal);
 
 });
+
+
+
+// Intialization code for restart button
+document.addEventListener('DOMContentLoaded', () => {
+  const restartButton = document.getElementById('restartButton');
+  restartButton.addEventListener('click', () => {
+    location.reload(); // Reloads the current page
+  });
+});
+
 
 
 // (Test) function for showing card decription
@@ -131,10 +219,12 @@ function populateCards() {
       `;
     });
 
+    });
     cardArea.appendChild(cardElement);
   });
 }
 */
+
 
 var cardList = startCards();
 
